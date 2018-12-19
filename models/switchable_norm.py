70275e12f21b53358510be91c7e9efa404b1d37c
Weili Shi <me@shi-weili.com>
2018-12-19 10:14:57 -0500

    Switchable Norm: Use moving average for instance norm and layer norm

--- models/switchable_norm.py
+++ models/switchable_norm.py
@@ -12,13 +12,13 @@ class SwitchNorm1d(nn.Module):
         self.bias = nn.Parameter(torch.zeros(1, num_features))
         self.mean_weight = nn.Parameter(torch.ones(2))
         self.var_weight = nn.Parameter(torch.ones(2))
-        self.register_buffer('running_mean', torch.zeros(1, num_features))
-        self.register_buffer('running_var', torch.zeros(1, num_features))
+        self.register_buffer('running_mean_bn', torch.zeros(1, num_features))
+        self.register_buffer('running_var_bn', torch.zeros(1, num_features))
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.running_mean.zero_()
-        self.running_var.zero_()
+        self.running_mean_bn.zero_()
+        self.running_var_bn.zero_()
         self.weight.data.fill_(1)
         self.bias.data.zero_()
 
@@ -36,16 +36,16 @@ class SwitchNorm1d(nn.Module):
             mean_bn = x.mean(0, keepdim=True)
             var_bn = x.var(0, keepdim=True)
             if self.using_moving_average:
-                self.running_mean.mul_(self.momentum)
-                self.running_mean.add_((1 - self.momentum) * mean_bn.data)
-                self.running_var.mul_(self.momentum)
-                self.running_var.add_((1 - self.momentum) * var_bn.data)
+                self.running_mean_bn.mul_(self.momentum)
+                self.running_mean_bn.add_((1 - self.momentum) * mean_bn.data)
+                self.running_var_bn.mul_(self.momentum)
+                self.running_var_bn.add_((1 - self.momentum) * var_bn.data)
             else:
-                self.running_mean.add_(mean_bn.data)
-                self.running_var.add_(mean_bn.data ** 2 + var_bn.data)
+                self.running_mean_bn.add_(mean_bn.data)
+                self.running_var_bn.add_(mean_bn.data ** 2 + var_bn.data)
         else:
-            mean_bn = torch.autograd.Variable(self.running_mean)
-            var_bn = torch.autograd.Variable(self.running_var)
+            mean_bn = torch.autograd.Variable(self.running_mean_bn)
+            var_bn = torch.autograd.Variable(self.running_var_bn)
 
         softmax = nn.Softmax(0)
         mean_weight = softmax(self.mean_weight)
@@ -74,16 +74,24 @@ class SwitchNorm2d(nn.Module):
         else:
             self.mean_weight = nn.Parameter(torch.ones(2))
             self.var_weight = nn.Parameter(torch.ones(2))
+        self.register_buffer('running_mean_in', torch.zeros(1, num_features, 1))
+        self.register_buffer('running_var_in', torch.zeros(1, num_features, 1))
+        self.register_buffer('running_mean_ln', torch.zeros(1, num_features, 1))
+        self.register_buffer('running_var_ln', torch.zeros(1, num_features, 1))
         if self.using_bn:
-            self.register_buffer('running_mean', torch.zeros(1, num_features, 1))
-            self.register_buffer('running_var', torch.zeros(1, num_features, 1))
+            self.register_buffer('running_mean_bn', torch.zeros(1, num_features, 1))
+            self.register_buffer('running_var_bn', torch.zeros(1, num_features, 1))
 
         self.reset_parameters()
 
     def reset_parameters(self):
+        self.running_mean_in.zero_()
+        self.running_var_in.zero_()
+        self.running_mean_ln.zero_()
+        self.running_var_ln.zero_()
         if self.using_bn:
-            self.running_mean.zero_()
-            self.running_var.zero_()
+            self.running_mean_bn.zero_()
+            self.running_var_bn.zero_()
         if self.last_gamma:
             self.weight.data.fill_(0)
         else:
@@ -99,28 +107,55 @@ class SwitchNorm2d(nn.Module):
         self._check_input_dim(x)
         N, C, H, W = x.size()
         x = x.view(N, C, -1)
-        mean_in = x.mean(-1, keepdim=True)
-        var_in = x.var(-1, keepdim=True)
 
-        mean_ln = mean_in.mean(1, keepdim=True)
-        temp = var_in + mean_in ** 2
-        var_ln = temp.mean(1, keepdim=True) - mean_ln ** 2
+        if self.training:
+            mean_in = x.mean(-1, keepdim=True)
+            var_in = x.var(-1, keepdim=True)
+
+            mean_ln = mean_in.mean(1, keepdim=True)
+            temp = var_in + mean_in ** 2
+            var_ln = temp.mean(1, keepdim=True) - mean_ln ** 2
+
+            if self.using_moving_average:
+                self.running_mean_in.mul_(self.momentum)
+                self.running_mean_in.add_((1 - self.momentum) * mean_in.data)
+                self.running_var_in.mul_(self.momentum)
+                self.running_var_in.add_((1 - self.momentum) * var_in.data)
+            
+                self.running_mean_ln.mul_(self.momentum)
+                self.running_mean_ln.add_((1 - self.momentum) * mean_ln.data)
+                self.running_var_ln.mul_(self.momentum)
+                self.running_var_ln.add_((1 - self.momentum) * var_ln.data)
+
+            else:
+                self.running_mean_in.add_(mean_in.data)
+                self.running_var_in.add_(mean_in.data ** 2 + var_in.data)
+
+                self.running_mean_ln.add_(mean_ln.data)
+                self.running_var_ln.add_(mean_ln.data ** 2 + var_ln.data)
+        
+        else:
+            mean_in = torch.autograd.Variable(self.running_mean_in)
+            var_in = torch.autograd.Variable(self.running_var_in)
+
+            mean_ln = torch.autograd.Variable(self.running_mean_ln)
+            var_ln = torch.autograd.Variable(self.running_var_ln)
 
         if self.using_bn:
             if self.training:
                 mean_bn = mean_in.mean(0, keepdim=True)
                 var_bn = temp.mean(0, keepdim=True) - mean_bn ** 2
                 if self.using_moving_average:
-                    self.running_mean.mul_(self.momentum)
-                    self.running_mean.add_((1 - self.momentum) * mean_bn.data)
-                    self.running_var.mul_(self.momentum)
-                    self.running_var.add_((1 - self.momentum) * var_bn.data)
+                    self.running_mean_bn.mul_(self.momentum)
+                    self.running_mean_bn.add_((1 - self.momentum) * mean_bn.data)
+                    self.running_var_bn.mul_(self.momentum)
+                    self.running_var_bn.add_((1 - self.momentum) * var_bn.data)
                 else:
-                    self.running_mean.add_(mean_bn.data)
-                    self.running_var.add_(mean_bn.data ** 2 + var_bn.data)
+                    self.running_mean_bn.add_(mean_bn.data)
+                    self.running_var_bn.add_(mean_bn.data ** 2 + var_bn.data)
             else:
-                mean_bn = torch.autograd.Variable(self.running_mean)
-                var_bn = torch.autograd.Variable(self.running_var)
+                mean_bn = torch.autograd.Variable(self.running_mean_bn)
+                var_bn = torch.autograd.Variable(self.running_var_bn)
 
         softmax = nn.Softmax(0)
         mean_weight = softmax(self.mean_weight)
@@ -156,15 +191,15 @@ class SwitchNorm3d(nn.Module):
             self.mean_weight = nn.Parameter(torch.ones(2))
             self.var_weight = nn.Parameter(torch.ones(2))
         if self.using_bn:
-            self.register_buffer('running_mean', torch.zeros(1, num_features, 1))
-            self.register_buffer('running_var', torch.zeros(1, num_features, 1))
+            self.register_buffer('running_mean_bn', torch.zeros(1, num_features, 1))
+            self.register_buffer('running_var_bn', torch.zeros(1, num_features, 1))
 
         self.reset_parameters()
 
     def reset_parameters(self):
         if self.using_bn:
-            self.running_mean.zero_()
-            self.running_var.zero_()
+            self.running_mean_bn.zero_()
+            self.running_var_bn.zero_()
         if self.last_gamma:
             self.weight.data.fill_(0)
         else:
@@ -192,16 +227,16 @@ class SwitchNorm3d(nn.Module):
                 mean_bn = mean_in.mean(0, keepdim=True)
                 var_bn = temp.mean(0, keepdim=True) - mean_bn ** 2
                 if self.using_moving_average:
-                    self.running_mean.mul_(self.momentum)
-                    self.running_mean.add_((1 - self.momentum) * mean_bn.data)
-                    self.running_var.mul_(self.momentum)
-                    self.running_var.add_((1 - self.momentum) * var_bn.data)
+                    self.running_mean_bn.mul_(self.momentum)
+                    self.running_mean_bn.add_((1 - self.momentum) * mean_bn.data)
+                    self.running_var_bn.mul_(self.momentum)
+                    self.running_var_bn.add_((1 - self.momentum) * var_bn.data)
                 else:
-                    self.running_mean.add_(mean_bn.data)
-                    self.running_var.add_(mean_bn.data ** 2 + var_bn.data)
+                    self.running_mean_bn.add_(mean_bn.data)
+                    self.running_var_bn.add_(mean_bn.data ** 2 + var_bn.data)
             else:
-                mean_bn = torch.autograd.Variable(self.running_mean)
-                var_bn = torch.autograd.Variable(self.running_var)
+                mean_bn = torch.autograd.Variable(self.running_mean_bn)
+                var_bn = torch.autograd.Variable(self.running_var_bn)
 
         softmax = nn.Softmax(0)
         mean_weight = softmax(self.mean_weight)
